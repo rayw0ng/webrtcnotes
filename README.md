cd172a87610d15222ade5ec5aaa5ea5e94a9251a
Ray Wang <raywang1980@qq.com>
2024-03-03 15:59:25 +0800

    RTCPeerConnection on one page

--- README.md
+++ README.md
@@ -9,6 +9,11 @@
 - localhost
 ## iframe
 
+# RTCPeerConnection
+## [track](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/track_event)
+## [addstream](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addstream_event)
+deprecated
+
 
 # [Pattern](https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation)
 ## [polite](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/signalingState)
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
+const pc = new RTCPeerConnection();
+const pc2 = new RTCPeerConnection();
 const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
 
 async function start() {
   try {
@@ -9,10 +12,35 @@ async function start() {
         audio: false
       }
       );
+    // trigger negotiationneeded event
+    for (const track of stream.getTracks()) {
+      pc.addTrack(track, stream);
+      pc2.addTrack(track, stream);
+    }
     localVideo.srcObject = stream;
+
+    await pc.setLocalDescription();
+    await pc2.setRemoteDescription(pc.localDescription);
+    await pc2.setLocalDescription();
+    await pc.setRemoteDescription(pc2.localDescription);
   } catch (err) {
     console.error(err);
   }
 }
-
+// associated tracks with stream before
+pc.ontrack = ({ streams }) => {
+  remoteVideo.srcObject = streams[0];
+};
+pc.onicecandidate = ({ candidate }) => {
+  if (candidate !== null) {
+    pc2.addIceCandidate(candidate);
+    console.log('send candidate', candidate);
+  }
+};
+pc2.onicecandidate = ({ candidate }) => {
+  if (candidate !== null) {
+    pc.addIceCandidate(candidate);
+    console.log('send candidate', candidate);
+  }
+};
 start();
